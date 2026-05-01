e08538cc1194f51911fa73093dbcc7ab2ee8b7fd
ekaskowi <elkaskowitz@gmail.com>
2026-04-30 23:59:16 -0700

    Added additional comments
    Added additional comments that I forgot for the IMU class

--- IMU.cpp
+++ IMU.cpp
@@ -1,18 +1,18 @@
 #include "IMU.h"
 
 void IMU::init() {
-  Wire2.begin();
+  Wire2.begin(); // Wire2 should correspond to SCL 24 and SDA 25
 
   if (_imu.begin() != ASM330LHH_OK) {
     Serial.println("IMU init failed");  //  I am unsure if printing errors to serial is how we want to debug, keeping a note here to ask
-    while (1); //                           Also unsure if freezing the program is a good idea here or maybe just disabling IMU functionality
+    while (1); // Also unsure if freezing the program is a good idea here, maybe we should just disable IMU functionality
   }
 
   _imu.Enable_X(); // accelerometer xyz
   _imu.Enable_G(); // gyro
 }
 
-IMU_Data IMU::read() {
+IMU_Data IMU::read() {  // Reminder to ask justin: do we want interupt driven data reading? Pin is on the pinout, but is unused in my logic
   _imu.Get_X_Axes(_data.accel);
   _imu.Get_G_Axes(_data.gyro);
 

1caa216d77f0166a3721d9ff65b2b0195fa2d28a
InterSigma123 <ealva163@ucr.edu>
2026-06-01 14:27:44 -0700

    Fixed compile error to make code compile.

--- daq.ino
+++ daq.ino
@@ -12,57 +12,57 @@
 // therm process: MCP96RL00
 
 // adc process: ADS8688IDBTR
-const short commandByte = 
-[0B1100000000000000, 0B1100010000000000, 0B1100100000000000, 0B1100110000000000];
+const u_int16_t commandByte = [0xC000, 0xC400, 0xC800, 0xCC00];
 // imu process: ASM330LHHXTR
 
 
 void setup() {
   // put your setup code here, to run once:
   // CAN RX and TX Pinouts
-  constexpr uint8_t TEENSY_RX_PIN =     23;
-  constexpr uint8_t TEENSY_TX_PIN =     22;
+  // constexpr uint8_t TEENSY_RX_PIN =     23;
+  // constexpr uint8_t TEENSY_TX_PIN =     22;
 
   // i2c pins
-  constexpr uint8_t SCL_IMU_PIN =       24;
-  constexpr uint8_t SDA_IMU_PIN =       25;
-  constexpr uint8_t INT_INU_PIN =       5;
-  constexpr uint8_t SCL_THERM_PIN =     19;
-  constexpr uint8_t SDA_THERM_PIN =     18;
+  // constexpr uint8_t SCL_IMU_PIN =       24;
+  // constexpr uint8_t SDA_IMU_PIN =       25;
+  // constexpr uint8_t INT_INU_PIN =       5;
+  // constexpr uint8_t SCL_THERM_PIN =     19;
+  // constexpr uint8_t SDA_THERM_PIN =     18;
 
   // GPS pins
-  constexpr uint8_t GPS_TX_PIN =        35;
-  constexpr uint8_t GPS_RX_PIN =        34;
+  // constexpr uint8_t GPS_TX_PIN =        35;
+  // constexpr uint8_t GPS_RX_PIN =        34;
 
   // digital pins
-  constexpr uint8_t DIG1_PIN =          29;
-  constexpr uint8_t DIG2_PIN =          30;
-  constexpr uint8_t DIG3_PIN =          31;
-  constexpr uint8_t DIG4_PIN =          32;
+  // constexpr uint8_t DIG1_PIN =          29;
+  // constexpr uint8_t DIG2_PIN =          30;
+  // constexpr uint8_t DIG3_PIN =          31;
+  // constexpr uint8_t DIG4_PIN =          32;
 
   // TELEM pins
-  constexpr uint8_t TELEM_RX_PIN =      7;
-  constexpr uint8_t TELEM_TX_PIN =      8;
+  // constexpr uint8_t TELEM_RX_PIN =      7;
+  // constexpr uint8_t TELEM_TX_PIN =      8;
 
   // ADC SPI pins
   constexpr uint8_t ADC_SCLK_PIN =      13;
   constexpr uint8_t ADC_SDI_PIN =       11; // MOSI
   constexpr uint8_t ADC_SDO_PIN =       12; // MISO
   constexpr uint8_t CS_PIN =            10;
-  constexpr uint8_t RST_PIN =           1;
+  // constexpr uint8_t RST_PIN =           1;
 
   // serial writing
-  serial.begin(115200);
+  Serial.begin(115200);
 
   // Initialize SPI pins
   pinMode(CS_PIN, OUTPUT);
   digitalWrite(CS_PIN, HIGH);
 
-  SPI.begin(ADC_SCLK_PIN, ADC_SDO_PIN, ADC_SDI_PIN, CS_PIN);
+  // Initializes SCLK 13, MOSI 11, MISO 12, and CS_PIN 10
+  SPI.begin();
 }
 
-int readShockPot(int channel) {
-  digitalWrite(CS_PIN, LOW);
+int readShockPot(int pin, int channel) {
+  digitalWrite(pin, LOW);
 
   // Send start bits to activate channel
   SPI.transfer16(commandByte[channel]);
@@ -71,7 +71,7 @@ int readShockPot(int channel) {
   // Assumes that device responds with 2 bytes after the command.
   short recievedData = SPI.transfer16(0x0000);
 
-  digitalWrite (CS_PIN, HIGH);
+  digitalWrite (pin, HIGH);
 
   return recievedData;
 }
@@ -82,7 +82,7 @@ void loop() {
   // collect adc data from pinouts
   short shockpot[4] = {};
   for (unsigned i = 0; i < 4; i++) {
-    shockpot[i] = readShockPot(i);
+    shockpot[i] = readShockPot(CS_PIN, i);
 
     // Testing purposes only
     Serial.print("Raw shock pot input ");
